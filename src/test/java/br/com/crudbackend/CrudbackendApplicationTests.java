d6bc8d46183648a28dbcb0c99ec81534c43fd5ba
stefanini <stefanini@STFBSBC044603-L.lan>
2021-11-20 13:25:42 -0300

    Primeiro commit com banco em memoria e entidade criada

---
+++ src/test/java/br/com/crudbackend/CrudbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crudbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
